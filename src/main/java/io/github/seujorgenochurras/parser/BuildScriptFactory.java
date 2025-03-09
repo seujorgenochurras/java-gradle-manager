97895b1d6416329d24248b5ac904bd0d4421f14f
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-03-08 22:17:21 -0300

    feat: create BuildScriptFactory

---
+++ src/main/java/io/github/seujorgenochurras/parser/BuildScriptFactory.java
@@ -0,0 +1,20 @@
+package io.github.seujorgenochurras.parser;
+
+import io.github.seujorgenochurras.domain.build.BuildScript;
+import io.github.seujorgenochurras.generate.antlr.KotlinLexer;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+public class BuildScriptFactory {
+
+    public static BuildScript fromKotlinScript(String rawScript) {
+        KotlinLexer kotlinLexer = new KotlinLexer(CharStreams.fromString(rawScript));
+        CommonTokenStream tokenStream = new CommonTokenStream(kotlinLexer);
+        return new KotlinBuildScriptParser().parse(tokenStream);
+    }
+
+    public static BuildScript fromGroovyScript(String rawScript) {
+        return null;
+    }
+
+}
