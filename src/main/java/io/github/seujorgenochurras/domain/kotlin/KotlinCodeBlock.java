30b81eed056a46cb1663e4dff0e15dae39d65ea1
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-03-08 22:09:42 -0300

    feat: add KotlinCodeBlock implementations

--- src/main/java/io/github/seujorgenochurras/domain/kotlin/KotlinCodeBlock.java
+++ src/main/java/io/github/seujorgenochurras/domain/kotlin/KotlinCodeBlock.java
@@ -1,19 +1,31 @@
 package io.github.seujorgenochurras.domain.kotlin;
 
 import io.github.seujorgenochurras.domain.CodeBlock;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.TokenStreamRewriter;
 
 public class KotlinCodeBlock implements CodeBlock {
 
     private TokenStreamRewriter rewriter;
+    private ParserRuleContext context;
 
-    @Override
-    public void addLine(String codeLine) {
+    public KotlinCodeBlock setRewriter(TokenStreamRewriter rewriter) {
+        this.rewriter = rewriter;
+        return this;
+    }
 
+    public KotlinCodeBlock setContext(ParserRuleContext context) {
+        this.context = context;
+        return this;
     }
 
     @Override
-    public String getCodeBlock() {
-        return "";
+    public void addLine(String codeLine) {
+        rewriter.insertAfter(getParent().start, "\n" + codeLine);
+    }
+
+    @Override
+    public ParserRuleContext getContext() {
+        return context;
     }
 }
