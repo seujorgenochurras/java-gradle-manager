f0f14cc16c5e08852e61854f1cdb3f8721f703ff
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-03-08 22:14:00 -0300

    feat: create FileUtils

---
+++ src/main/java/io/github/seujorgenochurras/util/FileUtils.java
@@ -0,0 +1,24 @@
+package io.github.seujorgenochurras.util;
+
+import java.io.*;
+import java.util.NoSuchElementException;
+
+public class FileUtils {
+    private FileUtils() {
+    }
+
+    public static String getFileAsString(File file) {
+        StringBuilder fileAsString = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            String currentLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                fileAsString.append(currentLine).append("\n");
+            }
+
+        } catch (IOException e) {
+            throw new NoSuchElementException(e);
+        }
+        return fileAsString.toString();
+    }
+}
