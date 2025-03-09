4516c162058395a78cfdcdb0ed75ea873cce6e28
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-03-08 22:09:35 -0300

    feat: add KotlinFunctionLiteral implementations

--- src/main/java/io/github/seujorgenochurras/domain/kotlin/KotlinFunctionLiteral.java
+++ src/main/java/io/github/seujorgenochurras/domain/kotlin/KotlinFunctionLiteral.java
@@ -1,20 +1,45 @@
 package io.github.seujorgenochurras.domain.kotlin;
 
+import io.github.seujorgenochurras.domain.CodeBlock;
 import io.github.seujorgenochurras.domain.FunctionLiteral;
+import io.github.seujorgenochurras.generate.antlr.KotlinParser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.TokenStreamRewriter;
 
 public class KotlinFunctionLiteral implements FunctionLiteral {
-    private String name;
-    private String codeBlock;
+    private final String identifier;
+    private final KotlinParser.FunctionLiteralContext context;
+    private CodeBlock codeBlock;
+    private final TokenStreamRewriter rewriter;
 
+    public KotlinFunctionLiteral(KotlinParser.FunctionLiteralContext context, String identifier, TokenStreamRewriter rewriter) {
+        this.context = context;
+        this.identifier = identifier;
+        this.rewriter = rewriter;
+    }
 
-
-    @Override
-    public String getName() {
-        return name;
+    private CodeBlock genCodeBlock() {
+        return new KotlinCodeBlock()
+                .setRewriter(rewriter)
+                .setContext(context.statements());
     }
 
     @Override
-    public String getBlock() {
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public CodeBlock getBlock() {
+        if (codeBlock == null) {
+            codeBlock = genCodeBlock();
+        }
         return codeBlock;
+    }
+
+
+    @Override
+    public ParserRuleContext getContext() {
+        return context;
     }
 }
