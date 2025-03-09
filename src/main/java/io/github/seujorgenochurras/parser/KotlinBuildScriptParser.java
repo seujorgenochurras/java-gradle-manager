56645b8ec6a8e59ee033455ba491ce0438e2cfce
seujorgenochurras <senhor_dois_monitores@outlook.com>
2025-03-08 22:17:10 -0300

    feat: create KotlinBuildScriptParser

---
+++ src/main/java/io/github/seujorgenochurras/parser/KotlinBuildScriptParser.java
@@ -0,0 +1,50 @@
+package io.github.seujorgenochurras.parser;
+
+import io.github.seujorgenochurras.domain.FunctionLiteral;
+import io.github.seujorgenochurras.domain.build.BuildScript;
+import io.github.seujorgenochurras.domain.build.KotlinBuildScript;
+import io.github.seujorgenochurras.domain.dependency.DependencyBlock;
+import io.github.seujorgenochurras.generate.antlr.KotlinParser;
+import io.github.seujorgenochurras.parser.listener.kotlin.KotlinFunctionListener;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStreamRewriter;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.util.HashMap;
+
+public class KotlinBuildScriptParser implements BuildScriptParser {
+
+    private KotlinParser kotlinParser;
+    private KotlinParser.ScriptContext scriptContext;
+    private TokenStreamRewriter rewriter;
+    private ParseTreeWalker walker;
+
+    private void init(CommonTokenStream tokenStream) {
+        this.kotlinParser = new KotlinParser(tokenStream);
+        this.scriptContext = kotlinParser.script();
+        this.walker = new ParseTreeWalker();
+        this.rewriter = new TokenStreamRewriter(tokenStream);
+    }
+
+    @Override
+    public BuildScript parse(CommonTokenStream tokenStream) {
+        init(tokenStream);
+
+        var functionLiterals = getFunctionLiterals();
+
+        FunctionLiteral dependencyFunctionLiteral = functionLiterals.get("dependencies");
+        DependencyBlock dependencyBlock = new DependencyBlock(dependencyFunctionLiteral);
+        KotlinBuildScript kotlinBuildScript = new KotlinBuildScript(rewriter);
+        kotlinBuildScript.setDependencyNode(dependencyBlock);
+
+        return kotlinBuildScript;
+    }
+
+    private HashMap<String, FunctionLiteral> getFunctionLiterals() {
+        var kotlinFunctionListener = new KotlinFunctionListener(rewriter);
+
+        walker.walk(kotlinFunctionListener, scriptContext);
+        return kotlinFunctionListener.getFunctionLiterals();
+
+    }
+}
---
+++ src/main/java/io/github/seujorgenochurras/parser/listener/kotlin/KotlinFunctionListener.java
@@ -0,0 +1,40 @@
+package io.github.seujorgenochurras.parser.listener.kotlin;
+
+import io.github.seujorgenochurras.domain.FunctionLiteral;
+import io.github.seujorgenochurras.domain.kotlin.KotlinFunctionLiteral;
+import io.github.seujorgenochurras.generate.antlr.KotlinParser;
+import io.github.seujorgenochurras.generate.antlr.KotlinParserBaseListener;
+import org.antlr.v4.runtime.TokenStreamRewriter;
+
+import java.util.HashMap;
+
+public class KotlinFunctionListener extends KotlinParserBaseListener {
+    private String lastIdentifier = "";
+    private final TokenStreamRewriter rewriter;
+
+    private final HashMap<String, FunctionLiteral> functionLiterals = new HashMap<>();
+
+    public HashMap<String, FunctionLiteral> getFunctionLiterals() {
+        return functionLiterals;
+    }
+
+    public KotlinFunctionListener(TokenStreamRewriter rewriter) {
+        this.rewriter = rewriter;
+    }
+
+    @Override
+    public void enterSimpleIdentifier(KotlinParser.SimpleIdentifierContext ctx) {
+        lastIdentifier = ctx.getText();
+    }
+
+    @Override
+    public void enterFunctionLiteral(KotlinParser.FunctionLiteralContext ctx) {
+        KotlinFunctionLiteral functionLiteral = new KotlinFunctionLiteral(ctx, lastIdentifier, rewriter);
+        functionLiterals.put(functionLiteral.getIdentifier(), functionLiteral);
+    }
+
+    @Override
+    public void exitFunctionLiteral(KotlinParser.FunctionLiteralContext ctx) {
+        lastIdentifier = "";
+    }
+}
